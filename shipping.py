52397f10de71169e7b76ae6ff84975f395a80ade
Rachel O'Dwyer <rachelodwyer@eduroam-100-36.nomadic.bris.ac.uk>
2022-03-29 14:08:55 +0100

    Add datasets for ships in 2019

---
+++ shipping.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+#DATASETS OF SHIP MOVEMENTS 15TH DAY OF EVERY MONTH 2019
+JAN2019 = pd.read_csv('AIS_2019_01_15.csv')
+print(type(JAN2019))
+
+FEB2019 = pd.read_csv('AIS_2019_02_15.csv')
+print(type(FEB2019))
+
+MAR2019 = pd.read_csv('AIS_2019_03_15.csv')
+print(type(MAR2019))
+
+APR2019 = pd.read_csv('AIS_2019_04_15.csv')
+print(type(APR2019))
+
+MAY2019 = pd.read_csv('AIS_2019_05_15.csv')
+print(type(MAY2019))
+
+JUN2019 = pd.read_csv('AIS_2019_06_15.csv')
+print(type(JUN2019))
+
+JUL2019 = pd.read_csv('AIS_2019_07_15.csv')
+print(type(JUL2019))
+
+AUG2019 = pd.read_csv('AIS_2019_08_15.csv')
+print(type(AUG2019))
+
+SEP2019 = pd.read_csv('AIS_2019_09_15.csv')
+print(type(SEP2019))
+
+OCT2019 = pd.read_csv('AIS_2019_10_15.csv')
+print(type(OCT2019))
+
+NOV2019 = pd.read_csv('AIS_2019_11_15.csv')
+print(type(NOV2019))
+
+DEC2019 = pd.read_csv('AIS_2019_12_15.csv')
+print(type(DEC2019))
