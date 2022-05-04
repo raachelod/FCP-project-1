c2026172b3a0cf094e678fb2e61c0f33317a8453
Rachel O'Dwyer <rachelodwyer@Rachels-MacBook-Air.local>
2022-05-04 14:34:00 +0100

    Adding input options

--- animation.py
+++ animation.py
@@ -12,24 +12,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
  
 # Total population, N.
-N = 1000
+N = int(input("Type your inital population, N"))
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0 = int(input("How many people initally infected?"))
+R0 = 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10
+#beta, gamma = 0.2, 1./10
+contact_rate = 0.2
+recovery_rate = 1./10
 # A grid of time points (in days)
 t_span = np.linspace(0, 160, 160)
 h = t_span[1]-t_span[0]
  
  
 # The SIR model differential equations.
-def deriv(y, N, beta, gamma):
+def deriv(y, N, contact_rate, recovery_rate):
     S, I, R = y
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
-    dRdt = gamma * I
+    dSdt = -contact_rate * S * I / N
+    dIdt = contact_rate * S * I / N - recovery_rate * I
+    dRdt = recovery_rate * I
     return np.array([dSdt, dIdt, dRdt])
  
  
@@ -60,7 +63,7 @@ def init():
  
 # animation function.  This is called sequentially
 def animate(i):
-    dX = deriv(X[:,i],N,beta,gamma)
+    dX = deriv(X[:,i],N,contact_rate,recovery_rate)
     X[:,i+1] = X[:,i] + h*dX
    
     line[0].set_data(t_span, X[0,:]/1000)
@@ -71,5 +74,13 @@ def animate(i):
 anim =FuncAnimation(fig, animate, init_func=init,
                                frames=len(t_span)-1, interval=20, blit=True)
 plt.show()
+
+filename = input("If you would like to save this animation, type your chosen filename. If not type 0.")
+
+if filename = "0":
+    plt.show()
+    
+else:
+    plt.savefig('"filename".pdf')
 
 ## 
