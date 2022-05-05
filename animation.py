bf6f4f1dc59f319b16015372a70576f8bce3efbe
Rachel O'Dwyer <rachelodwyer@eduroam-46-30.nomadic.bris.ac.uk>
2022-05-05 14:39:56 +0100

    updating

--- animation.py
+++ animation.py
@@ -10,20 +10,55 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
- 
+import argparse
+
+parser = argparse.ArgumentParser(description= 'Model an epidemic.')
+parser.add_argument('--population', metavar=('N'), type=int, default=1000, 
+                    help=('Inital population at start of simulate'))
+parser.add_argument('--infected', metavar=('I0'), type=int, default = 1,
+                    help=('Initial number of infected people'))
+parser.add_argument('--duration', metavar=('T'), type=int, default=1095,
+                    help=('Simulate for T days.'))
+parser.add_argument('--file', metavar='N', type=str, default=None,
+                    help='Filename to save to instead of showing on screen')
+parser.add_argument('--plot', action='store_true',
+                    help='Generate plots instead of an animation')
+args = parser.parse_args(args)     
+
+#if args.plot:
+ #  fig = plot_simulation(simulation, args.duration)
+#
+ #  if args.file is None:
+  #          #  python simulator.py --plot
+   #    plt.show()
+   #else:
+    #   #  python simulator.py --plot --file=plot.pdf
+    #   fig.savefig(args.file)
+#else:
+ ##   animation = Animation(simulation, args.duration)
+   
+   # if args.file is None:
+    #        #  python simulator.py
+      #      animation.show()
+   # else:
+            #  python simulator.py --file=animation.mp4
+            #
+            # NOTE: this needs ffmpeg to be installed.
+    #        animation.save(args.file)            
+
 # Total population, N.
-N = int(input("Type your inital population, N"))
+N = int(input("Type your inital population, N")) #population - command line argument
 # Initial number of infected and recovered individuals, I0 and R0.
-I0 = int(input("How many people initally infected?"))
+I0 = int(input("How many people initally infected?"))  #infection - command line argument
 R0 = 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-#beta, gamma = 0.2, 1./10
+# Contact rate, contact_rate, and mean recovery rate, recovery_rate, (in 1/days).
+#contact_rate, recovery_rate = 0.2, 1./10
 contact_rate = 0.2
 recovery_rate = 1./10
 # A grid of time points (in days)
-t_span = np.linspace(0, 160, 160)
+t_span = np.linspace(0, 365, 365) #timespan over a year
 h = t_span[1]-t_span[0]
  
  
@@ -61,7 +96,7 @@ def init():
     [line[i].set_data([], []) for i in range(3)]
     return line
  
-# animation function.  This is called sequentially
+#animation function.  This is called sequentially
 def animate(i):
     dX = deriv(X[:,i],N,contact_rate,recovery_rate)
     X[:,i+1] = X[:,i] + h*dX
@@ -73,6 +108,16 @@ def animate(i):
  
 anim =FuncAnimation(fig, animate, init_func=init,
                                frames=len(t_span)-1, interval=20, blit=True)
+
+
+#creating grid animation 
+#potentially create barchart
+#create a specific animation that simulates the specific values from 'England' data 
+
+
+    
+    
+
 plt.show()
 
 
