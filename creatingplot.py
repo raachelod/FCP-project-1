2cfb119ef8dccaa0bc3fbe30b8f5d9844f51f5bd
Rachel O'Dwyer <rachelodwyer@eduroam-46-30.nomadic.bris.ac.uk>
2022-05-05 15:06:43 +0100

    editing pd names

--- creatingplot.py
+++ creatingplot.py
@@ -7,8 +7,7 @@ Created on Thu Apr 28 18:13:07 2022
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-import proplot as pplt
-#import argparse 
+
 
 #reading excel files 
 APR20AUG20 = pd.read_excel('Apr20-Aug20.xlsx', index_col = 'Name', parse_dates= True, thousands= ',')
@@ -18,13 +17,13 @@ AUG21APR22 = pd.read_excel('Aug21-Apr22.xlsx', index_col = 'Name', parse_dates= 
                                           
 #merging data by year
 APR20APR21 = pd.merge(APR20AUG20, AUG20APR21, on= 'Name')
-APR21APR22 = pd.merge(APR21NOV21, NOV21APR22, on= 'Name')
+APR21APR22 = pd.merge(APR21AUG21, AUG21APR22, on= 'Name')
 APR20APR22 =pd.merge(APR20APR21,APR21APR22, on = 'Name')
 
 print(APR20APR22)
 
 #saving the merged data file to reuse and/or view seperately 
-#APR20APR22.to_csv('merged_data.csv')
+APR20APR22.to_csv('merged_data.csv')
 
 #make this a class - different types of plots
 #plotting data
@@ -51,16 +50,12 @@ plt.xlabel('Months during COVID-19')
 plt.ylabel('Number of hospital admissions')
 plt.legend()
    
-
-<<<<<<< HEAD
-
 plot2 = plt.figure(2)
 plt.plot(southwest, color = 'grey' )
 plt.title('Hospital admissions during COVID-19 in South West England')
 plt.xlabel('Months during COVID-19')
 plt.ylabel('Number of hospital admissions')
 plt.savefig('southwest_data.pdf')
-=======
->>>>>>> 947f76c5cce6c3897f95ec63f1b3d1de6ce2c938
+
 
 plt.show()
